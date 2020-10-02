388ff0f3a6d834aa5c767a517fd1bcd840fba701
Saaket Poray <saaketporay@gmail.com>
2020-10-02 15:27:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ client.once('ready', () => {
     console.log('Space Information Bot is online!')
 })
 
-
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+      msg.reply('Pong!');
+    }
+  });
 
 
 client.login(config.token);
