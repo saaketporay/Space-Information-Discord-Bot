7da310f7f58935e69be82991d8e6e8ffaf33b45a
Tahmeed_Ahmed <40477982+ChaoticCross12@users.noreply.github.com>
2020-10-04 03:34:49 +0600

    Added APOD command

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const config = require('./config.json');
+const fetch = require('node-fetch');
 
 
 const client = new Discord.Client();
@@ -7,14 +8,28 @@ const client = new Discord.Client();
 
 
 client.once('ready', () => {
-    console.log('Space Information Bot is online!')
-})
+    console.log('Space Information Bot is online!');
+});
 
-client.on('message', msg => {
+client.on('message', async msg => {
     if (msg.content === 'ping') {
-      msg.reply('Pong!');
+        msg.reply('Pong!');
     }
-  });
+
+    else if (msg.content === 'apod') {
+        let getApod = async () => {
+
+            let result = await fetch
+            ('https://api.nasa.gov/planetary/apod?api_key=jZ7JIaKjGCMnOlPwqFryi0cfXaenexyWAJPXePcb')
+
+            let json = await result.json()
+            return json
+        }
+
+        let apodValue = await getApod();
+        msg.reply("The Link is : " + apodValue.hdurl)
+    }
+});
 
 
 client.login(config.token);
